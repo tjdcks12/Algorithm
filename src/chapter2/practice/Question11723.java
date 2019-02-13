d40bcf8225520590e000bb8771819ba3f84e4be3
강성찬 <tjdcks_12@naver.com>
2019-02-13 15:34:17 +0900

    챕터 2 문제 풀이

---
+++ src/chapter2/practice/Question11723.java
@@ -0,0 +1,85 @@
+package chapter2.practice;
+
+import java.util.Scanner;
+
+public class Question11723 {
+
+
+    public static void main(String args[]) {
+        int loop;
+        String command;
+
+        Scanner sc = new Scanner(System.in);
+        BitMask bitMask = new BitMask();
+
+
+        loop = sc.nextInt();
+
+        for(int i=0; i<loop; i++){
+            command = sc.next();
+
+
+            switch(command){
+                case "add":
+                    bitMask.add(sc.nextInt());
+                    break;
+                case "remove":
+                    bitMask.remove(sc.nextInt());
+                    break;
+                case "check":
+                    bitMask.check(sc.nextInt());
+                    break;
+                case "toggle":
+                    bitMask.toggle(sc.nextInt());
+                    break;
+                case "all":
+                    bitMask.all();
+                    break;
+                case "empty":
+                    bitMask.empty();
+                    break;
+            }
+        }
+
+        bitMask.getResult();
+    }
+}
+
+class BitMask{
+    private int number = 0;
+    private StringBuilder result = new StringBuilder();
+
+
+    public void add(int x) {
+        this.number = (this.number | 1 << x);
+    }
+
+    public void remove(int x) {
+        this.number = (this.number & ~(1 << x));
+    }
+
+    public void check(int x) {
+        if((this.number & (1 << x)) == 0){
+            result.append("0\n");
+        } else{
+            result.append("1\n");
+
+        }
+    }
+
+    public void toggle(int x) {
+        this.number = this.number ^ (1<<x);
+    }
+
+    public void all() {
+        this.number = (1<<21) - 1;
+    }
+
+    public void empty() {
+        this.number = 0;
+    }
+
+    public void getResult() {
+        System.out.println(this.result.toString());
+    }
+}
